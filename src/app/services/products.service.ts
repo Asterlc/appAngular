f6fb8daaae1458a9dd497e47bea83129d5ab29a1
Lucas Aster <asterlc102@gmail.com>
2022-02-26 13:49:09 -0300

    fixes

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -10,7 +10,7 @@ import { Observable, ObservedValueOf } from "rxjs";
 export class ProductsService {
   constructor(private http: HttpClient) { };
 
-  protected baseURL = `http://localhost:3000/`;
+  protected baseURL = `http://localhost:3000`;
 
   getProducts(): Observable<Product[]> {
     const product = this.http.get<Product[]>(`${this.baseURL}/products`);
