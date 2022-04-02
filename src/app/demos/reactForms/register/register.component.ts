e66fa91e21cd1cbfd21b737a40e20ce6fb180947
Lucas Aster <asterlc102@gmail.com>
2022-04-02 17:21:21 -0300

    basic react form validators

--- src/app/demos/reactForms/register/register.component.ts
+++ src/app/demos/reactForms/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/userModel';
 
 @Component({
@@ -10,21 +10,28 @@ import { User } from 'src/app/models/userModel';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   user: User;
+  formResult: string;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      name: [''],
-      document: [''],
-      email: [''],
-      password: [''],
-      passwordConfirm: [''],
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      document: ['', [Validators.required, Validators.maxLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      passwordConfirm: ['', Validators.required],
     })
+    // console.log('this.registerForm', this.registerForm)
   }
 
   createUser() {
-    this.user = Object.assign({}, this.user, this.registerForm.value);
+    if (this.registerForm.dirty && this.registerForm.valid) {
+      this.user = Object.assign({}, this.user, this.registerForm.value);
+      this.formResult = JSON.stringify(this.registerForm.value);
+      window.alert('Dados enviados com sucesso')
+    } else {
+      window.alert('Preencha os campos obrigatórios')
+    }
   }
-
 }
