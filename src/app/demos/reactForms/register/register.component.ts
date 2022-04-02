b2b3a50ea8b5b217e69983861f54084e075ed2da
Lucas Aster <asterlc102@gmail.com>
2022-04-02 20:49:45 -0300

    async validation

--- src/app/demos/reactForms/register/register.component.ts
+++ src/app/demos/reactForms/register/register.component.ts
@@ -1,22 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { NgBrazilValidators } from 'ng-brazil';
 import { utilsBr } from 'js-brasil';
 import { User } from 'src/app/models/userModel';
 import { CustomValidators } from 'ng2-validation';
+import { DisplayMessage, GenericFormValidator, ValidationMessages } from 'src/app/helpers/generic-form-validation';
+import { fromEvent, merge, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, AfterViewInit {
+  //Gerar uma queryList, obter coleção de itens do HTML.
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+
   registerForm: FormGroup;
   user: User;
   formResult: string;
   MASKS = utilsBr.MASKS;
 
-  constructor(private fb: FormBuilder) { }
+  validationMessages: ValidationMessages;
+  genericValidator: GenericFormValidator;
+  displayMessage: DisplayMessage = {};
+
+  constructor(private fb: FormBuilder) {
+    this.validationMessages = {
+      name: {
+        required: 'Campo obrigatório',
+        minlength: 'O nome precisa ter no mínimo 5 caracteres', //Ambos atribuidos pelo Validator do angular/forms
+        maxlength: 'O nome precisa ter no máximo 150 caracteres',
+      },
+      document: {
+        required: 'Campo obrigatório',
+        cpf: 'CPF em formato inválido' //Atribuido pelo NgBrazilValidator
+      },
+      email: {
+        required: 'Campo obrigatório',
+        email: 'Formato inválido' //Atribuido pelo validator do angular/forms
+      },
+      password: {
+        required: 'Campo obrigatório',
+        rangeLength: 'Deve conter entre 5 e 15 caracteres'  //Atribuido pelo CustomValidator
+      },
+      passwordConfirm: {
+        required: 'Campo obrigatório', //Atribuido pelo Validator do angular/forms
+        rangeLength: 'Deve conter entre 5 e 15 caracteres', //Atribuido pelo CustomValidator
+        equalTo: 'Senhas estão diferentes' //Atribuido pelo CustomValidator
+      }
+    }
+
+    this.genericValidator = new GenericFormValidator(this.validationMessages);
+  }
 
   ngOnInit(): void {
     let pwd = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
@@ -31,6 +67,18 @@ export class RegisterComponent implements OnInit {
     })
     console.log('this.registerForm', this.registerForm)
   }
+
+  //Implementado após o documento HTML for devolvido ao usuário
+  ngAfterViewInit(): void {
+    //Mapeia os elementos em foco utilizado no formulário.
+    let controlBlurs: Observable<any>[] = this.formInputElements
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    //Irá realizar merge para todos os focos disponíveis no formulário.  
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.genericValidator.processMessage(this.registerForm);
+    });
+  };
 
   createUser() {
     if (this.registerForm.dirty && this.registerForm.valid) {
