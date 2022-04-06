b78c042268f3b706025a75ad5182592156f60f80
Lucas Aster <asterlc102@gmail.com>
2022-04-06 18:36:17 -0300

    links management

--- src/app/navigation/menu/menu.component.ts
+++ src/app/navigation/menu/menu.component.ts
@@ -1,7 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Nav } from './linkInterface';
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html', 
+  templateUrl: './menu.component.html',
 })
-export class MenuComponent { }
+export class MenuComponent {
+  nav: Nav[] = [
+    {
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/register',
+      name: 'Registro',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/product-list',
+      name: 'Produtos',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/about',
+      name: 'Sobre',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/contact',
+      name: 'Contatos',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/features-data-binding',
+      name: 'Funcionalidades',
+      exact: true,
+      admin: false
+    },
+  ]
+
+}
