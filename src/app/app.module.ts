c196c362e883d088f7a58583cde9c98558b486f6
Lucas Aster <asterlc102@gmail.com>
2022-04-15 23:52:15 -0300

    formatting app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
+//FROM ANGULAR
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app.routes';
-import { APP_BASE_HREF } from '@angular/common';
-import { DataBindingComponent } from './demos/data-binding/data-binding.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from "@angular/common/http";
-
-import { registerLocaleData } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//APP COMPONENTS
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { RegisterComponent } from './demos/reactForms/register/register.component';
+//SERVICES
+import { AuthGuardService } from './services/guards/app.guard';
+import { RegisterGuard } from './services/guards/register.guard';
+import { ProductsService } from 'src/app/services/products.service';
+//REGISTER LOCALE FOR PIPES
 import localePT from '@angular/common/locales/pt';
 registerLocaleData(localePT);
+//MÓDULOS CRIADOS
+import { NavigationModule } from './navigation/navigationModule';
+import { AboutModule } from './about/about.module';
+//LIBS INSIDE ANGULAR
 import { NgBrazil } from 'ng-brazil';
 import { TextMaskModule } from 'angular2-text-mask';
 import { CustomFormsModule } from 'ng2-validation';
-import { NavigationModule } from './navigation/navigationModule';
-import { AboutModule } from './about/about.module';
-import { ProductsService } from 'src/app/services/products.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AuthGuardService } from './services/guards/app.guard';
-import { RegisterComponent } from './demos/reactForms/register/register.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { RegisterGuard } from './services/guards/register.guard';
-import { MoviesComponent } from './movies/movies.component';
+
 @NgModule({
   declarations: [
     AppComponent,
